fc13f8da4601e4f1643270e6aa50a7ea621d28a4
T-vK <t-vk@users.noreply.github.com>
2019-08-05 08:00:00 +0000

    fix inputMouse bug

--- BleConnectionStatus.cpp
+++ BleConnectionStatus.cpp
@@ -6,12 +6,12 @@ BleConnectionStatus::BleConnectionStatus(BLECharacteristic* inputMouse) {
 
 void BleConnectionStatus::onConnect(BLEServer* pServer) {
   this->connected = true;
-  BLE2902* desc = (BLE2902*)inputMouse->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
+  BLE2902* desc = (BLE2902*)this->inputMouse->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
   desc->setNotifications(true);
 }
 
 void BleConnectionStatus::onDisconnect(BLEServer* pServer) {
   this->connected = false;
-  BLE2902* desc = (BLE2902*)inputMouse->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
+  BLE2902* desc = (BLE2902*)this->inputMouse->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
   desc->setNotifications(false);
 }
