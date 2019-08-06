13be5bd692bae50932bb318005f323b5cb47a6fe
T-vK <t-vk@users.noreply.github.com>
2019-08-06 08:00:00 +0000

    Remove unnecessary comments

--- BleMouse.cpp
+++ BleMouse.cpp
@@ -19,63 +19,6 @@
 static const char* LOG_TAG = "BLEDevice";
 #endif
 
-/*
-static const uint8_t _hidReportDescriptor[] = {
-  USAGE_PAGE(1),       0x01,
-  USAGE(1),            0x02,
-  COLLECTION(1),       0x01,
-  REPORT_ID(1),        0x01,
-  USAGE(1),            0x01,
-  COLLECTION(1),       0x00,
-  USAGE_PAGE(1),       0x09,
-  USAGE_MINIMUM(1),    0x1,
-  USAGE_MAXIMUM(1),    0x3,
-  LOGICAL_MINIMUM(1),  0x0,
-  LOGICAL_MAXIMUM(1),  0x1,
-  REPORT_COUNT(1),     0x3,
-  REPORT_SIZE(1),      0x1,
-  0x80|0x01,           0x2,    // (Data, Variable, Absolute), ;3 button bits
-  REPORT_COUNT(1),     0x1,
-  REPORT_SIZE(1),      0x5,
-  0x80|0x01,           0x1,    //(Constant), ;5 bit padding
-  USAGE_PAGE(1),       0x1,    //(Generic Desktop),
-  USAGE(1),            0x30,
-  USAGE(1),            0x31,
-  USAGE(1),            0x38,
-  LOGICAL_MINIMUM(1),  0x81,
-  LOGICAL_MAXIMUM(1),  0x7f,
-  REPORT_SIZE(1),      0x8,
-  REPORT_COUNT(1),     0x3,
-  0x80|0x01,           0x6,    //(Data, Variable, Relative), ;2 position bytes (X & Y)
-  END_COLLECTION(0),
-  END_COLLECTION(0)
-};*/
-
-//
-// Wheel Mouse - simplified version
-//
-// Input report - 5 bytes
-//
-// Byte | D7 D6 D5 D4 D3 D2 D1 D0
-// ------+---------------------------------------------------------------------
-// 0 | 0 0 0 Forward Back Middle Right Left (Button)
-// 1 | X
-// 2 | Y
-// 3 | Vertical Wheel
-// 4 | Horizontal (Tilt) Wheel
-//
-// Feature report - 1 byte
-//
-// Byte | D7 D6 D5 D4 | D3 D2 | D1 D0
-// ------+------------------------------+--------------+----------------
-// 0 | 0 0 0 0 | Horizontal | Vertical
-// (Resolution multiplier)
-//
-// Reference
-// Wheel.docx in "Enhanced Wheel Support in Windows Vista" on MS WHDC
-// http://www.microsoft.com/whdc/device/input/wheel.mspx
-//
-
 static const uint8_t _hidReportDescriptor[] = {
   0x05, 0x01, // USAGE_PAGE (Generic Desktop)
   0x09, 0x02, // USAGE (Mouse)
@@ -148,7 +91,6 @@ static const uint8_t _hidReportDescriptor[] = {
   0xc0, // END_COLLECTION
   0xc0 // END_COLLECTION
 };
-
 
 BleMouse::BleMouse(void) : _buttons(0)
 {
