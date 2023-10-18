010cfaa90ade4d1516be98d625c83d7ee3e68581
Pavlo Sharhan <pavel.shargan@gmail.com>
2023-10-18 16:15:00 -0400

    Update BleMouse.cpp Modified data types to ensure compatibility with ESP32-S3, including converting from std::string to String where necessary.

--- BleMouse.cpp
+++ BleMouse.cpp
@@ -7,10 +7,10 @@
 #include "HIDKeyboardTypes.h"
 #include <driver/adc.h>
 #include "sdkconfig.h"
-
+​
 #include "BleConnectionStatus.h"
 #include "BleMouse.h"
-
+​
 #if defined(CONFIG_ARDUHAL_ESP_LOG)
   #include "esp32-hal-log.h"
   #define LOG_TAG ""
@@ -18,7 +18,7 @@
   #include "esp_log.h"
   static const char* LOG_TAG = "BLEDevice";
 #endif
-
+​
 static const uint8_t _hidReportDescriptor[] = {
   USAGE_PAGE(1),       0x01, // USAGE_PAGE (Generic Desktop)
   USAGE(1),            0x02, // USAGE (Mouse)
@@ -59,7 +59,7 @@ static const uint8_t _hidReportDescriptor[] = {
   END_COLLECTION(0),         //   END_COLLECTION
   END_COLLECTION(0)          // END_COLLECTION
 };
-
+​
 BleMouse::BleMouse(std::string deviceName, std::string deviceManufacturer, uint8_t batteryLevel) : 
     _buttons(0),
     hid(0)
@@ -69,16 +69,16 @@ BleMouse::BleMouse(std::string deviceName, std::string deviceManufacturer, uint8
   this->batteryLevel = batteryLevel;
   this->connectionStatus = new BleConnectionStatus();
 }
-
+​
 void BleMouse::begin(void)
 {
   xTaskCreate(this->taskServer, "server", 20000, (void *)this, 5, NULL);
 }
-
+​
 void BleMouse::end(void)
 {
 }
-
+​
 void BleMouse::click(uint8_t b)
 {
   _buttons = b;
@@ -86,7 +86,7 @@ void BleMouse::click(uint8_t b)
   _buttons = 0;
   move(0,0,0,0);
 }
-
+​
 void BleMouse::move(signed char x, signed char y, signed char wheel, signed char hWheel)
 {
   if (this->isConnected())
@@ -101,7 +101,7 @@ void BleMouse::move(signed char x, signed char y, signed char wheel, signed char
     this->inputMouse->notify();
   }
 }
-
+​
 void BleMouse::buttons(uint8_t b)
 {
   if (b != _buttons)
@@ -110,64 +110,64 @@ void BleMouse::buttons(uint8_t b)
     move(0,0,0,0);
   }
 }
-
+​
 void BleMouse::press(uint8_t b)
 {
   buttons(_buttons | b);
 }
-
+​
 void BleMouse::release(uint8_t b)
 {
   buttons(_buttons & ~b);
 }
-
+​
 bool BleMouse::isPressed(uint8_t b)
 {
   if ((b & _buttons) > 0)
     return true;
   return false;
 }
-
+​
 bool BleMouse::isConnected(void) {
   return this->connectionStatus->connected;
 }
-
+​
 void BleMouse::setBatteryLevel(uint8_t level) {
   this->batteryLevel = level;
   if (hid != 0)
       this->hid->setBatteryLevel(this->batteryLevel);
 }
-
+​
 void BleMouse::taskServer(void* pvParameter) {
   BleMouse* bleMouseInstance = (BleMouse *) pvParameter; //static_cast<BleMouse *>(pvParameter);
   BLEDevice::init(bleMouseInstance->deviceName);
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(bleMouseInstance->connectionStatus);
-
+​
   bleMouseInstance->hid = new BLEHIDDevice(pServer);
   bleMouseInstance->inputMouse = bleMouseInstance->hid->inputReport(0); // <-- input REPORTID from report map
   bleMouseInstance->connectionStatus->inputMouse = bleMouseInstance->inputMouse;
-
+​
   bleMouseInstance->hid->manufacturer()->setValue(bleMouseInstance->deviceManufacturer);
-
+​
   bleMouseInstance->hid->pnp(0x02, 0xe502, 0xa111, 0x0210);
   bleMouseInstance->hid->hidInfo(0x00,0x02);
-
+​
   BLESecurity *pSecurity = new BLESecurity();
-
+​
   pSecurity->setAuthenticationMode(ESP_LE_AUTH_BOND);
-
+​
   bleMouseInstance->hid->reportMap((uint8_t*)_hidReportDescriptor, sizeof(_hidReportDescriptor));
   bleMouseInstance->hid->startServices();
-
+​
   bleMouseInstance->onStarted(pServer);
-
+​
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
   pAdvertising->setAppearance(HID_MOUSE);
   pAdvertising->addServiceUUID(bleMouseInstance->hid->hidService()->getUUID());
   pAdvertising->start();
   bleMouseInstance->hid->setBatteryLevel(bleMouseInstance->batteryLevel);
-
+​
   ESP_LOGD(LOG_TAG, "Advertising started!");
   vTaskDelay(portMAX_DELAY); //delay(portMAX_DELAY);
 }
